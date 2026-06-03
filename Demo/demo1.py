d2b868a519eff1f4eab69d6b7e68fac81410410a
xywekil <3488974026@qq.com>
2026-06-03 23:58:18 +0800

    第一周已完成

---
+++ Demo/demo1.py
@@ -0,0 +1,8 @@
+i = 0
+while i < 10:
+    i += 1
+print(i)
+b=0
+for a in range(1,101):
+    b+=a
+print(b)
---
+++ Demo/demo2.py
@@ -0,0 +1,14 @@
+def add(a,b):
+    if a>b:
+        return a
+    else:
+        return b
+def jo(a):
+    if a%2==0:
+        print("偶数")
+    else:
+        print("奇数")
+
+print(add(4,6))
+c=int(input())
+jo(c)
