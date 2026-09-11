04ea321fb46eed38c730376bfdd951bf4eba0c37
xjz-ekil <3488974026@qq.com>
2026-09-12 02:24:10 +0800

    Delete Demo directory

--- Demo/demo.py
+++
@@ -1,8 +0,0 @@
-print("请输入你的成绩：")
-score=int(input())
-if score >= 90:
-    print("你的成绩很优秀")
-elif score >= 60:
-    print("你的成绩及格啦")
-else:
-    print("你的成绩不及格")
--- Demo/demo1.py
+++
@@ -1,8 +0,0 @@
-i = 0
-while i < 10:
-    i += 1
-print(i)
-b=0
-for a in range(1,101):
-    b+=a
-print(b)
